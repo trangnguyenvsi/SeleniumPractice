dab5a092546e5c5b84261d8845d78c4df3335a2d
Trang Le <Trang Le@192.168.1.111>
2016-03-21 22:24:30 +0700

    common

--- src/main/java/com/vsii/tsc/guru/commethods/CommonMethods.java
+++ src/main/java/com/vsii/tsc/guru/commethods/CommonMethods.java
@@ -1,6 +1,9 @@
 package com.vsii.tsc.guru.commethods;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.vsii.tsc.guru.pages.method.LoginPageMethod;
 import com.vsii.tsc.utility.TestBase;
@@ -13,5 +16,15 @@ public class CommonMethods {
 		if(TestBase.driver.findElements(By.name("uid")).size()!=0){
 			objLogin.loginToManagerPage("mngr30380", "vAtepYs");
 		}
+	}
+	
+	public static void waitUntil(WebElement webElement) {
+		WebDriverWait wait = new WebDriverWait(TestBase.driver, 30);
+		wait.until(ExpectedConditions.elementToBeClickable(webElement));
+	}
+	
+	public static void waitUntilElementLocated(String elementLocated) {
+		WebDriverWait wait = new WebDriverWait(TestBase.driver, 30);
+		 wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(elementLocated)));
 	}
 }
