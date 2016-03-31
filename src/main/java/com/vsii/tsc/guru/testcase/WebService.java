fa56bf042316555d92f96301eb9a0dbc97b96363
quyentx <quyentx@outlook.com>
2016-03-31 10:58:53 +0700

    Automatically create projects in OpenERP, optimizing with JIRA

--- src/main/java/com/vsii/tsc/guru/testcase/WebService.java
+++ src/main/java/com/vsii/tsc/guru/testcase/WebService.java
@@ -2,6 +2,7 @@ package com.vsii.tsc.guru.testcase;
 
 import java.io.IOException;
 
+import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -11,6 +12,7 @@ import com.vsii.tsc.guru.pages.method.JiraDashBoardMethod;
 import com.vsii.tsc.guru.pages.method.JiraLoginPageMethod;
 import com.vsii.tsc.guru.pages.method.JiraProjectDetailsPageMethod;
 import com.vsii.tsc.guru.pages.method.LoginPageMethod;
+import com.vsii.tsc.guru.pages.method.ProjectCreateNewMethod;
 import com.vsii.tsc.guru.pages.method.ProjectsPageMethod;
 import com.vsii.tsc.guru.pages.method.TasksPageMethod;
 import com.vsii.tsc.guru.pages.method.VSIIProjectPageMethod;
@@ -27,6 +29,7 @@ public class WebService {
 	JiraDashBoardMethod objJiraDashboard;
 	JiraProjectDetailsPageMethod objJiraProjectDetail;
 	JiraBrowseProjectPageMethod objJiraBrowseProject;
+	ProjectCreateNewMethod objErpProject;
 	VSIIProjectPageMethod objVSIIProject;
 	String username;
 	String password;
@@ -42,6 +45,7 @@ public class WebService {
 		objJiraDashboard = new JiraDashBoardMethod(TestBase.driver);
 		objJiraProjectDetail = new JiraProjectDetailsPageMethod(TestBase.driver);
 		objJiraBrowseProject = new JiraBrowseProjectPageMethod(TestBase.driver);
+		objErpProject = new ProjectCreateNewMethod(TestBase.driver);
 		objVSIIProject = new VSIIProjectPageMethod(TestBase.driver);
 		objService.clickProjectMenu();
 
@@ -56,11 +60,16 @@ public class WebService {
 	 * 
 	 * Create new web service with the following info:
 	 * 
-	 * - Name < define> - User <define> - Webservice Protocol= http - Webservice
-	 * Host=< ip of JIRA> - Webserver Post= < port of JIRA> - WebService Path :
-	 * /path/to/JIRA/issues.json? status_id=* - Model Name :
-	 * vsii_project.vsii_JIRA_connector - Decode Method Name:
-	 * collect_project_worklog - Authentication: Basic; Account=JIRA admin;
+	 * Name < define>
+	 * User <define>
+	 * Webservice Protocol= http
+	 * Webservice Host=< ip of JIRA>
+	 * Webserver Port= < port of JIRA>
+	 * WebService Path: /path/to/JIRA/issues.json? status_id=*
+	 * Model Name: vsii_project.vsii_JIRA_connector
+	 * Decode Method Name: collect_project_worklog
+	 * Authentication: Basic; 
+	 * Account=JIRA admin;
 	 * password= JIRA password
 	 * 
 	 * Save new web service and verify if it has been created
@@ -134,17 +143,35 @@ public class WebService {
 	 */
 
 	@Test(priority = 3, description = "Verify the ability of mapping imported project from JIRA with existed OpenERP project work correctly", dataProvider = "W03", dataProviderClass = TestData.class)
-	public void W03(String projectName, String expectedTaskKey) throws InterruptedException {
+	public void W03(String projectName, String approvedEffort, String projectType, String commDetails, String expectedTaskKey) throws InterruptedException {
 		// Method name
 		TestBase.methodName = "W03";
 		
-		// Perform test steps
-		objService.clickVsiiProject();
-		objService.clickImportedProject();
-		objVSIIProject.clickEditImportedProject();
-		objVSIIProject.setProjectName(projectName);
-		objVSIIProject.clickNoVerifyChkbox();
-		objVSIIProject.clickSaveImportedProject();
+		// Verify if an empty OpenERP project has been created		
+		objProject.clickProjectLink();
+		Thread.sleep(1000);
+		objProject.chooseDepartment();
+		String xpath="//td[@data-field='name' and contains(text(),'"+projectName+"')]";
+		int size = TestBase.driver.findElements(By.xpath(xpath)).size();
+		if(size>0){
+			// Perform test steps
+			objService.clickVsiiProject();
+			objService.clickImportedProject();
+			objVSIIProject.clickEditImportedProject();
+			objVSIIProject.setProjectName(projectName);
+			objVSIIProject.clickNoVerifyChkbox();
+			objVSIIProject.clickSaveImportedProject();
+		}else{
+			// if required project is not exist, create a new one
+			objErpProject.createProject(projectName, approvedEffort, projectType, commDetails);
+			// Perform test steps
+			objService.clickVsiiProject();
+			objService.clickImportedProject();
+			objVSIIProject.clickEditImportedProject();
+			objVSIIProject.setProjectName(projectName);
+			objVSIIProject.clickNoVerifyChkbox();
+			objVSIIProject.clickSaveImportedProject();
+		}
 		
 		// Verify imported info
 		objProject.clickProjectLink();
@@ -156,29 +183,100 @@ public class WebService {
 
 	}
 
+	
+	/*
+	 * 1. Check if JIRA project is imported successfully in to VSIIProject
+	 * if (yes){
+	 * 		Run service and verify result
+	 * 		} else {
+	 * 			Check if the required empty OpenERP project has been created
+	 * 			if (yes) {
+	 * 				Edit VSIIProject to map the newly created one with the imported one
+	 * 				Run service and verify result
+	 * 			}else{
+	 * 			Create new project
+	 * 			Edit VSIIProject to map the newly created one with the imported one
+	 * 			Run service and verify result
+	 * 		}
+	 */
 	@Test(priority = 4, description = "Verify importing JIRA project to OpenERP project work correctly", dataProvider = "W04", dataProviderClass = TestData.class)
-	public void W04(String timeSpent, String doneBy) throws InterruptedException {
+	public void W04(String projectName, String approvedEffort, String projectType, String commDetails, String timeSpent, String doneBy) throws InterruptedException {
 		// Method name
 		TestBase.methodName = "W04";
 
-		// Perform test steps
-		objService.clickWebServiceOption();
-		objService.clickChooseService();
-		objService.clickRunningService();
+		//Check if JIRA project is imported successfully in to VSIIProject
 		objService.clickVsiiProject();
-		objService.clickImportedProject();
-
-		objProject.clickProjectLink();
+//		objService.clickImportedProject();
+		String xpath2="//td[@data-field='project_id' and contains(text(),'"+projectName+"')]";
+		int size2 = TestBase.driver.findElements(By.xpath(xpath2)).size();
+		if(size2>0){
+			// Perform test steps
+			objService.clickWebServiceOption();
+			objService.clickChooseService();
+			objService.clickRunningService();
+		}else{
+			objProject.clickProjectLink();
+			Thread.sleep(1000);
+			objProject.chooseDepartment();
+			String xpath="//td[@data-field='name' and contains(text(),'"+projectName+"')]";
+			int size = TestBase.driver.findElements(By.xpath(xpath)).size();
+			if(size>0){
+				//Edit imported project in Project/VSIIProject 
+				objService.clickVsiiProject();
+				objService.clickImportedProject();
+				objVSIIProject.clickEditImportedProject();
+				objVSIIProject.setProjectName(projectName);
+				objVSIIProject.clickNoVerifyChkbox();
+				objVSIIProject.clickSaveImportedProject();
+				
+				// Run service 
+				objService.clickWebServiceOption();
+				objService.clickChooseService();
+				objService.clickRunningService();
+				
+			}else{
+				// if required project is not exist, create a new one
+				objErpProject.createProject(projectName, approvedEffort, projectType, commDetails);
+				
+				//Edit imported project in Project/VSIIProject 
+				objService.clickVsiiProject();
+				objService.clickImportedProject();
+				objVSIIProject.clickEditImportedProject();
+				objVSIIProject.setProjectName(projectName);
+				objVSIIProject.clickNoVerifyChkbox();
+				objVSIIProject.clickSaveImportedProject();
+				
+				// Run service 
+				objService.clickWebServiceOption();
+				objService.clickChooseService();
+				objService.clickRunningService();
+				
+			}
+		}
+		
+		// Verify test result
+		objProject.clickProjectLink();	// Click on Project section in left side-bar
 		objProject.chooseDepartment();
 		objProject.chooseTestProject();
 		objProject.clickTaskBtn();
-
 		objTasks.clickLatestTask();
-
-		// Verify test result
 		Assert.assertEquals(objTasks.getTimeSpent(), timeSpent);
 		Assert.assertEquals(objTasks.getDoneBy(), doneBy);
 	}
+	
+	/*
+	 * Check if JIRA project is existed 
+	 * if(yes){
+	 * 		Check if Execute Test task has been created
+	 * 		if(yes){
+	 * 			Check if any worklog has been recorded on the task
+	 * 			if((yes){
+	 * 				
+	 * 				} 
+	 * 			}
+	 * 
+	 * 		}
+	 */
 
 	@Test(priority = 5, description = "Verify that when add new worklog in JIRA project webservice importing work correctly", dataProvider = "W05", dataProviderClass = TestData.class)
 	public void W05(String jiraBaseUrl, String username, String password, String timeSpent, String startTime,
@@ -212,6 +310,7 @@ public class WebService {
 		objProject.chooseDepartment();
 		objProject.chooseTestProject();
 		objProject.clickTaskBtn();
+		Thread.sleep(500);
 		objTasks.clickLatestTask();
 		Thread.sleep(500);
 		
@@ -308,3 +407,7 @@ public class WebService {
 	}
 
 }
+
+
+
+
