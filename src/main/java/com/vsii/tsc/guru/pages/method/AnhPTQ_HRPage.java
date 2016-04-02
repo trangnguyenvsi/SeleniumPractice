752403b4c2e77e25dd8eaff5300b676c02568a1f
anhptq <anh.qaq@gmail.com>
2016-04-02 09:41:46 +0700

    anhptq commit

---
+++ src/main/java/com/vsii/tsc/guru/pages/method/AnhPTQ_HRPage.java
@@ -0,0 +1,105 @@
+package com.vsii.tsc.guru.pages.method;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class AnhPTQ_HRPage {
+	@FindBy(xpath="html/body/div[1]/table/tbody/tr[3]/td[1]/div[1]/div[5]/ul[3]/li[4]/a/span")
+	private WebElement mnMyTimeSheet;
+	
+	@FindBy(xpath="html/body/div[1]/table/tbody/tr[3]/td[2]/div/div/div/div[1]/div/table/thead/tr/th[2]/div")
+	private WebElement ttlEmployee;
+	
+	@FindBy(xpath="html/body/div[1]/table/tbody/tr[3]/td[2]/div/div/div/div[1]/div/table/thead/tr/th[3]/div")
+	private WebElement ttlDateFrom;
+	
+	@FindBy(xpath="html/body/div[1]/table/tbody/tr[3]/td[2]/div/div/div/div[1]/div/table/thead/tr/th[4]/div")
+	private WebElement ttlDateTo;
+	
+	@FindBy(xpath="html/body/div[1]/table/tbody/tr[3]/td[2]/div/div/div/div[1]/div/table/thead/tr/th[5]/div")
+	private WebElement ttlDepartment;
+	
+	@FindBy(xpath="html/body/div[1]/table/tbody/tr[3]/td[2]/div/div/div/div[1]/div/table/thead/tr/th[6]/div")
+	private WebElement ttlTotalTimesheet;
+	
+	@FindBy(xpath="html/body/div[1]/table/tbody/tr[3]/td[2]/div/div/div/div[1]/div/table/thead/tr/th[7]/div")
+	private WebElement ttlStatus;
+	
+	@FindBy(xpath="html/body/div[1]/table/tbody/tr[3]/td[2]/div/div/table/tbody/tr[2]/td[1]/div/div/button[1]")
+	private WebElement btnCreate;
+	
+	@FindBy(xpath="html/body/div[1]/table/tbody/tr[3]/td[2]/div/div/table/tbody/tr[2]/td[1]/div/div/span/a[2]")
+	private WebElement btnImport;
+	
+	public WebElement getTtlDateFrom() {
+		return ttlDateFrom;
+	}
+
+	public void setTtlDateFrom(WebElement ttlDateFrom) {
+		this.ttlDateFrom = ttlDateFrom;
+	}
+
+	public WebElement getTtlDateTo() {
+		return ttlDateTo;
+	}
+
+	public void setTtlDateTo(WebElement ttlDateTo) {
+		this.ttlDateTo = ttlDateTo;
+	}
+
+	public WebElement getTtlDepartment() {
+		return ttlDepartment;
+	}
+
+	public void setTtlDepartment(WebElement ttlDepartment) {
+		this.ttlDepartment = ttlDepartment;
+	}
+
+	public WebElement getTtlTotalTimesheet() {
+		return ttlTotalTimesheet;
+	}
+
+	public void setTtlTotalTimesheet(WebElement ttlTotalTimesheet) {
+		this.ttlTotalTimesheet = ttlTotalTimesheet;
+	}
+
+	public WebElement getTtlStatus() {
+		return ttlStatus;
+	}
+
+	public void setTtlStatus(WebElement ttlStatus) {
+		this.ttlStatus = ttlStatus;
+	}
+
+	public WebElement getBtnCreate() {
+		return btnCreate;
+	}
+
+	public void setBtnCreate(WebElement btnCreate) {
+		this.btnCreate = btnCreate;
+	}
+
+	public WebElement getBtnImport() {
+		return btnImport;
+	}
+
+	public void setBtnImport(WebElement btnImport) {
+		this.btnImport = btnImport;
+	}
+
+	public WebElement getTtlEmployee() {
+		return ttlEmployee;
+	}
+
+	public void setTtlEmployee(WebElement ttlEmployee) {
+		this.ttlEmployee = ttlEmployee;
+	}
+
+	public WebElement getMnMyTimeSheet() {
+		return mnMyTimeSheet;
+	}
+
+	public void setMnMyTimeSheet(WebElement mnMyTimeSheet) {
+		this.mnMyTimeSheet = mnMyTimeSheet;
+	}
+}
---
+++ src/main/java/com/vsii/tsc/guru/pages/method/AnhPTQ_MainPage.java
@@ -0,0 +1,51 @@
+package com.vsii.tsc.guru.pages.method;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class AnhPTQ_MainPage {
+	@FindBy(xpath="html/body/div[1]/table/tbody/tr[2]/td/ul/li[5]/a/span")
+	private WebElement mnHR;
+
+	@FindBy(xpath="html/body/div[1]/table/tbody/tr[2]/td/ul/li[3]/a/span")
+	private WebElement mnHRPMO;
+	
+	@FindBy(xpath="html/body/div[1]/table/tbody/tr[2]/td/span")
+	private WebElement drpdwnMenu;
+	
+	@FindBy(xpath="html/body/div[1]/table/tbody/tr[2]/td/span/ul/li[5]/a")
+	private WebElement btnLogout;
+	
+	public WebElement getDrpdwnMenu() {
+		return drpdwnMenu;
+	}
+
+	public void setDrpdwnMenu(WebElement drpdwnMenu) {
+		this.drpdwnMenu = drpdwnMenu;
+	}
+
+	public WebElement getBtnLogout() {
+		return btnLogout;
+	}
+
+	public void setBtnLogout(WebElement btnLogout) {
+		this.btnLogout = btnLogout;
+	}
+
+	public WebElement getMnHRPMO() {
+		return mnHRPMO;
+	}
+
+	public void setMnHRPMO(WebElement mnHRPMO) {
+		this.mnHRPMO = mnHRPMO;
+	}
+
+	public WebElement getMnHR() {
+		return mnHR;
+	}
+
+	public void setMnHR(WebElement mnHR) {
+		this.mnHR = mnHR;
+	}
+
+}
