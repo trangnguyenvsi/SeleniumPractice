09b10996078c78c294e4358673c81b8776f9c7a5
anhptq <Quynhanh1!>
2016-04-19 21:38:45 +0700

    Resolved confilict CommonMehthods

--- src/main/java/com/vsii/tsc/guru/commethods/CommonMethods.java
+++ src/main/java/com/vsii/tsc/guru/commethods/CommonMethods.java
@@ -1,8 +1,12 @@
 package com.vsii.tsc.guru.commethods;
 
+import java.util.List;
+
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.vsii.tsc.guru.pages.method.JiraLoginPageMethod;
@@ -57,5 +61,60 @@ public class CommonMethods {
 	// Click on Jira home
 	public static void clickJiraHome(){
 		TestBase.driver.findElement(By.id("home_link")).click();
+	}
+	
+	//Check column should not contain duplicate value
+	public static boolean colShouldNotDuplicated (WebElement column) {
+		boolean isNotDuplicate = true;
+		List<WebElement> columns = column.findElements(By.tagName("td"));
+		for (int rowNum = 0; rowNum < columns.size()-1; rowNum++) {
+			for (int i = 0; i < (columns.size() - rowNum); i++) {
+				if(columns.get(rowNum).getText() == columns.get(i).getText()) {
+					isNotDuplicate = false;
+					break;
+				}
+			}
+		}
+		return isNotDuplicate;
+	}
+	
+	//Check column should not contain blank value
+	public static boolean colShouldNotBlank (WebElement column) {
+		boolean isNotBlank = true;
+		List<WebElement> columns = column.findElements(By.tagName("td"));
+		for (int rowNum = 0; rowNum < columns.size(); rowNum++) {
+			if(columns.get(rowNum).getText().trim().isEmpty()) {
+				isNotBlank = false;
+			}
+		}
+		return isNotBlank;
+	}
+	
+	//Accept alert
+	public static void acceptPopup () {
+		Alert alert = TestBase.driver.switchTo().alert();
+		alert.accept();
+	}
+	
+	//Cancel alert
+	public static void cancelPopup () {
+		Alert alert = TestBase.driver.switchTo().alert();
+		alert.dismiss();
+	}
+	
+	//Get alert message
+	public static String getAlertMessage () {
+		Alert alert = TestBase.driver.switchTo().alert();
+		return alert.getText();
+	}
+	
+	//Select option in select box by contains value
+	public static void selectOptionByContains(String text, Select selectBox, List<WebElement> optionsInnerText) {
+		for(WebElement option: optionsInnerText){
+			String textContent = option.getText();
+			if(textContent.toLowerCase().contains(text.toLowerCase())) {
+				selectBox.selectByVisibleText(textContent);
+			}
+		}
 	}
 }
