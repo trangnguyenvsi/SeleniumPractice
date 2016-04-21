6508038e895455eb320e0311144e3c967800b29b
diuttm <tran.thi.minh.diu@vsi-international.com>
2016-04-21 10:33:53 +0700

    1111

--- src/main/java/com/vsii/tsc/guru/testdata/TestData.java
+++ src/main/java/com/vsii/tsc/guru/testdata/TestData.java
@@ -277,4 +277,167 @@ public class TestData {
     	Object[][] data = ExcelHandle.getTable("./data/SkillsManagement_TestData.xlsx", "Employee", "SelectedEmployee");
     	return data;
     }
+    @DataProvider(name="TC04")
+    public static Object[][] TC04() {
+        Object[][] data = ExcelHandle.getTable("./data/ISMS_TestData.xlsx", "Login", "TC04");
+        return data;
+    }
+    @DataProvider(name="TC05")
+    public static Object[][] TC05() {
+        Object[][] data = ExcelHandle.getTable("./data/ISMS_TestData.xlsx", "Login", "TC05");
+        return data;
+    }
+    @DataProvider(name="TC07")
+    public static Object[][] TC07() {
+        Object[][] data = ExcelHandle.getTable("./data/ISMS_TestData.xlsx", "Login", "TC07");
+        return data;
+    }
+    @DataProvider(name="TC08")
+    public static Object[][] TC08() {
+        Object[][] data = ExcelHandle.getTable("./data/ISMS_TestData.xlsx", "Login", "TC08");
+        return data;
+    }
+    @DataProvider(name="TC09")
+    public static Object[][] TC09() {
+        Object[][] data = ExcelHandle.getTable("./data/ISMS_TestData.xlsx", "Login", "TC09");
+        return data;
+    }
+    @DataProvider(name="TC12")
+    public static Object[][] TC12() {
+        Object[][] data = ExcelHandle.getTable("./data/ISMS_TestData.xlsx", "ISMSType", "TC12");
+        return data;
+    }
+    @DataProvider(name="TC13")
+    public static Object[][] TC13() {
+        Object[][] data = ExcelHandle.getTable("./data/ISMS_TestData.xlsx", "Login", "TC13");
+        return data;
+    }
+    @DataProvider(name="TC15")
+    public static Object[][] TC15() {
+        Object[][] data = ExcelHandle.getTable("./data/ISMS_TestData.xlsx", "Login", "TC15");
+        return data;
+    }
+    @DataProvider(name="TC18")
+    public static Object[][] TC18() {
+        Object[][] data = ExcelHandle.getTable("./data/ISMS_TestData.xlsx", "Login", "TC18");
+        return data;
+    }
+    @DataProvider(name="TC19")
+    public static Object[][] TC19() {
+        Object[][] data = ExcelHandle.getTable("./data/ISMS_TestData.xlsx", "Login", "TC19");
+        return data;
+    }
+    @DataProvider(name="Adminlogin")
+    public static Object[][] getLoginValid1() {
+        Object[][] data = ExcelHandle.getTable("./data/ISMS_TestData.xlsx", "Login", "TC02");
+        return data;
+    }
+    
+    @DataProvider(name="notAdminlogin")
+    public static Object[][] getLoginnotAdmin() {
+        Object[][] data = ExcelHandle.getTable("./data/ISMS_TestData.xlsx", "Login", "TC03");
+        return data;
+    }
+    @DataProvider(name="ISMSSelectType")
+    public static Object[][] ISMSSelectType() {
+        Object[][] data = ExcelHandle.getTable("./data/ISMS_TestData.xlsx", "ISMSType", "TC10");
+        return data;
+    }
+    @DataProvider(name = "ISMSAssginIssue")
+    public static Object[][] ISMSAssginIssue() {
+        Object[][] data = ExcelHandle.getTable("./data/ISMS_TestData.xlsx", "ISMSType", "TC11");
+        return data;
+    }
+    @DataProvider(name="ISMSAssginSearch")
+    public static Object[][]  ISMSAssginSearch() {
+        Object[][] data = ExcelHandle.getTable("./data/ISMS_TestData.xlsx", "ISMSType", "TC16");
+        return data;
+    }
+    
+    @DataProvider(name="LogAnote")
+    public static Object[][]  LogAnote() {
+        Object[][] data = ExcelHandle.getTable("./data/ISMS_TestData.xlsx", "ISMSType", "TC20");
+        return data;
+        }
+    
+    @DataProvider(name="approveAnote")
+        public static Object[][]  approveAnote() {
+            Object[][] data = ExcelHandle.getTable("./data/ISMS_TestData.xlsx", "ISMSType", "TC21");
+            return data;
+    }
+    @DataProvider(name="TC23")
+    public static Object[][]  TC23() {
+        Object[][] data = ExcelHandle.getTable("./data/ISMS_TestData.xlsx", "ISMSType", "TC23");
+        return data;
+}
+    @DataProvider(name="TC27")
+    public static Object[][]  TC27() {
+        Object[][] data = ExcelHandle.getTable("./data/ISMS_TestData.xlsx", "ISMSType", "TC27");
+        return data;
+}
+    @DataProvider(name="loginTask")
+    public static Object[][]  loginTask() {
+        Object[][] data = ExcelHandle.getTable("./data/ISMS_TestData.xlsx", "ISMSType", "TC28");
+        return data;
+    }
+    @DataProvider(name="createATask")
+    public static Object[][]  createATask() {
+        Object[][] data = ExcelHandle.getTable("./data/ISMS_TestData.xlsx", "CreateTask", "TC30");
+        return data;
+    }
+    @DataProvider(name="phantich")
+    public static Object[][]  phantich() {
+        Object[][] data = ExcelHandle.getTable("./data/ISMS_TestData.xlsx", "CreateTask", "TC32");
+        return data;
+    }
+       @DataProvider(name="TC31")
+        public static Object[][]  TC31() {
+            Object[][] data = ExcelHandle.getTable("./data/ISMS_TestData.xlsx", "ISMSType", "TC31");
+            return data;
+        }
+    @DataProvider(name="giaiphap")
+    public static Object[][]  giaiphap() {
+        Object[][] data = ExcelHandle.getTable("./data/ISMS_TestData.xlsx", "CreateTask", "TC33");
+        return data;
+    }
+       @DataProvider(name="duyet")
+        public static Object[][]  duyet() {
+            Object[][] data = ExcelHandle.getTable("./data/ISMS_TestData.xlsx", "CreateTask", "TC34");
+            return data;
+        }
+    @DataProvider(name = "kiemtra1")
+    public static Object[][] kiemtra1() {
+        Object[][] data = ExcelHandle.getTable("./data/ISMS_TestData.xlsx", "CreateTask", "TC36");
+        return data;
+    }
+    @DataProvider(name = "Asset Categories")
+    public static Object[][] AssetCategories() {
+        Object[][] data = ExcelHandle.getTable("./data/ISMS_TestData.xlsx", "Login", "TC38");
+        return data;
+    }
+    @DataProvider(name = "Asset Categories 1")
+    public static Object[][] AssetCategories1() {
+        Object[][] data = ExcelHandle.getTable("./data/ISMS_TestData.xlsx", "Login", "TC39");
+        return data;
+    }
+    @DataProvider(name = "Create Asset Categories")
+    public static Object[][] CreateAssetCategories() {
+        Object[][] data = ExcelHandle.getTable("./data/ISMS_TestData.xlsx", "Login", "TC40");
+        return data;
+    }
+    @DataProvider(name = "Create Asset Company")
+    public static Object[][] CreateAssetCompany() {
+        Object[][] data = ExcelHandle.getTable("./data/ISMS_TestData.xlsx", "Login", "TC41");
+        return data;
+    }
+    @DataProvider(name = "Create Asset Company 2")
+    public static Object[][] CreateAssetCompany2() {
+        Object[][] data = ExcelHandle.getTable("./data/ISMS_TestData.xlsx", "Login", "TC42");
+        return data;
+    }
+    @DataProvider(name = "Create Asset Company 3")
+    public static Object[][] CreateAssetCompany3() {
+        Object[][] data = ExcelHandle.getTable("./data/ISMS_TestData.xlsx", "Login", "TC43");
+        return data;
+    }
 }
